db34269fdf142dece90ea60c9b8b16540e5e74e0
Stanislau Trebnikau <stasonpokemon@icloud.com>
2022-11-11 20:57:44 +0300

    add date of birthday and create time;

--- src/main/java/com/spring/mvc/entity/Person.java
+++ src/main/java/com/spring/mvc/entity/Person.java
@@ -6,6 +6,9 @@ import javax.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -33,5 +36,14 @@ public class Person {
     // Belarus, Vitebsk, 210001
     @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}", message = "Your address should be in this format: Country, City, Postal Code (6 digits)")
     private String address;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_of_birth")
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date dateOfBirthday;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "create_at")
+    private Date createAt;
 
 }
--- src/main/java/com/spring/mvc/service/PersonService.java
+++ src/main/java/com/spring/mvc/service/PersonService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +28,7 @@ public class PersonService {
 
     @Transactional
     public Person save(Person person) {
+        person.setCreateAt(new Date());
         return personRepo.save(person);
     }
 
